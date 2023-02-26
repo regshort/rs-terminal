2f4b909c239b7b46d01a048fb49f4504c8a23e81
GREEB <gr44bli@gmail.com>
2023-02-26 14:59:52 +0100

    style()

--- components/alphaWarning.tsx
+++ components/alphaWarning.tsx
@@ -1,9 +1,18 @@
-import Link from "next/link"
-
 const Alphawarning = () => {
   return (
-    <div className=" p-1 flex relative text-[14px] mx-auto duration-150 font-mono text-center justify-center items-center bg-black">
-      <p className="text-white font-bold mb-0 py-1 rounded flex ">
+    <div className="pb-5 flex relative text-[14px] mx-auto duration-150 font-mono text-center justify-center items-center ">
+      {/* <div
+        style={{
+          mixBlendMode: 'screen',
+          filter: 'contrast(250%) brightness(600%) invert(100%)',
+          background:
+            'linear-gradient(to bottom, #770D14 -150%, transparent), url(/static/images/noise.svg)',
+          //   background:
+          //     ' linear-gradient(to bottom, , transparent), url(https://grainy-gradients.vercel.app/noise.svg);',
+        }}
+        className="-z-10 absolute h-[350px] w-full opacity-40"
+      ></div> */}
+      <p className="font-bold  px-2 py-1 rounded flex ">
         Closed Beta for regShort Terminal starting soon{" "}
       </p>
     </div>
