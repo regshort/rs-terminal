804e504ace2fa835eb32463e2ede6d8059d8e5c2
GREEB <gr44bli@gmail.com>
2023-02-28 03:11:42 +0100

    chore()

--- next.config.js
+++ next.config.js
@@ -1,38 +1,35 @@
 /** @type {import('next').NextConfig} */
-const withTM = require("next-transpile-modules")(["gutils"]);
-const PerspectivePlugin = require("@finos/perspective-webpack-plugin");
-const removeImports = require("next-remove-imports")();
+const PerspectivePlugin = require("@finos/perspective-webpack-plugin")
+const removeImports = require("next-remove-imports")()
 
-const nextConfig = withTM({
+const nextConfig = {
   i18n: {
     locales: ["en"],
-    defaultLocale: "en",
+    defaultLocale: "en"
   },
   experimental: { esmExternals: true },
   reactStrictMode: false,
   swcMinify: true,
-  images:{
-    domains:  [process.env.NEXT_PUBLIC_CDN]
+  images: {
+    domains: [process.env.NEXT_PUBLIC_CDN]
   },
   compiler: {
-    removeConsole: process.env.NODE_ENV !== 'development',
+    removeConsole: process.env.NODE_ENV !== "development"
   },
   webpack(config) {
-    config.plugins.push(
-      new PerspectivePlugin()
-    );
+    config.plugins.push(new PerspectivePlugin())
     config.module.rules.push({
       test: /\.svg$/,
       use: ["@svgr/webpack"]
-    });
-    return config;
+    })
+    return config
   }
+}
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true
 })
 
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  register: true,
-});
-
-module.exports = removeImports(withPWA(nextConfig));
+module.exports = removeImports(withPWA(nextConfig))
