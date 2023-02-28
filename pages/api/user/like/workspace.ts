230bc0ab24122a4e552396e72a7b632398223a7e
greeb <gr44bli@gmail.com>
2023-02-28 22:42:17 +0100

    fix(likedworkspace): upadatedat was wrong

--- pages/api/user/like/workspace.ts
+++ pages/api/user/like/workspace.ts
@@ -19,6 +19,7 @@ export default async function handler(req: any, res: any) {
               workspace: true,
               watchlist: true,
               default: true,
+              updatedAt: true,
               globalDefault: true,
               relativeDate: true,
               private: true,
